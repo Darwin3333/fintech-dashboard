f6af1adf1119d19c683ac9ef5d911eb7e1536152
Emanuel <emanuelbrunobn@rede.ulbra.br>
2025-04-07 17:13:08 -0300

    feat : alterando titulo do dom dinamicamente

--- project-fintech/src/Components/Header.tsx
+++ project-fintech/src/Components/Header.tsx
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DateRange from '../Context/DateRange';
 import Meses from './Meses';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
   const [title, setTitle] = useState('Resumo');
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      setTitle('Resumo');
+      document.title = 'Fintech | Resumo';
+    } else if (location.pathname === '/vendas') {
+      setTitle('Vendas');
+      document.title = 'Fintech | Vendas';
+    }
+  }, [location]);
+
   return (
     <header className="mb">
       <div className="daterange mb">
