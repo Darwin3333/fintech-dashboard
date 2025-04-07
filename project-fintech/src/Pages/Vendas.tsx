4070c4905b26fbd5afc3008718e0bcc66b4a4145
Emanuel <emanuelbrunobn@rede.ulbra.br>
2025-04-06 22:15:21 -0300

    feat : criado componente Vendas e VendaItem

---
+++ project-fintech/src/Pages/Vendas.tsx
@@ -0,0 +1,20 @@
+import { useData } from '../Context/DataContext';
+import VendaItem from '../Components/VendaItem';
+
+const Vendas = () => {
+  const { data } = useData();
+
+  if (data === null) return null;
+  return (
+    <ul>
+      {data.map((venda) => (
+        <li>
+          {' '}
+          <VendaItem key={venda.id} venda={venda} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Vendas;
