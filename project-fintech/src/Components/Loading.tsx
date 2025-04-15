ab5cc417dd6525e374025edae4286315c29ffa2c
brunoNasc <emanuelbrunobn@rede.ulbra.br>
2025-04-14 22:57:01 -0300

    fix : Corrigido loading animado

--- project-fintech/src/Components/Loading.tsx
+++ project-fintech/src/Components/Loading.tsx
@@ -1,5 +1,30 @@
+import React, { CSSProperties } from 'react';
+
+const style: CSSProperties = {
+  border: 'var(--gap-s) solid var(--color-3)',
+  borderRightColor: 'var(--color-4)',
+  marginBottom: 'var(--gap-s)',
+  width: 'var(--gap)',
+  height: 'var(--gap)',
+  borderRadius: '50%',
+  animation: 'spin 1s infinite',
+};
+
 const Loading = () => {
-  return <strong>Aguarde,Carregando</strong>;
+  return (
+    <div style={style}>
+      <style>
+        {`
+        @keyframes spin {
+        to {
+            transform: rotate(360deg);
+            }
+        }
+        
+        `}
+      </style>
+    </div>
+  );
 };
 
 export default Loading;
